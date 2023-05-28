5712a39850ba9d7babba877b8a1af2e8bb8e7de1
skydread1 <loic.blancharddev@gmail.com>
2023-05-28 14:50:22 +0800

    Add post_delete url, template, view and styling

--- loicblog/blog/views.py
+++ loicblog/blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import CreatePostForm, EditPostForm
+from django.urls import reverse_lazy
 
 ## Class-Based Views (CBVs)
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class PostDetailsView(DetailView):
     model = Post
@@ -22,3 +24,8 @@ class EditPostView(UpdateView):
     model = Post
     form_class = EditPostForm
     template_name = 'edit_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
