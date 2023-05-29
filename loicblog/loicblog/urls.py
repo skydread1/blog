41b378c76e89dfe2c36333ea5753937dcaaab884
skydread1 <loic.blancharddev@gmail.com>
2023-05-29 08:29:42 +0800

    Fix members/register path

--- loicblog/loicblog/urls.py
+++ loicblog/loicblog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('blog.urls')),
     path('members/', include('django.contrib.auth.urls')), # django built in auth urls: login/logout
-    path('members', include('members.urls')), # if not django auth urls, go for our custom app urls: register
+    path('members/', include('members.urls')), # if not django auth urls, go for our custom app urls: register
 ]
