f367aaee0dfd447d8ff474c2663318bf804459c1
skydread1 <loic.blancharddev@gmail.com>
2023-06-26 22:55:51 +0800

    Fix the error for when there is no category yet

--- loicblog/blog/forms.py
+++ loicblog/blog/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Post, Category
 from markdownx.fields import MarkdownxFormField
 
-choices = Category.objects.all().values_list('name', 'name')
-choice_list = [choice for choice in choices]
 
 class CreatePostForm(forms.ModelForm):
+    category = forms.ChoiceField(choices=[])
+
     class Meta:
         model = Post
         fields = '__all__'
@@ -13,17 +13,26 @@ class CreatePostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'placeholder': 'My post title'}),
             'author': forms.Select(),
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'category': forms.Select(choices=choice_list),
             'body': MarkdownxFormField(),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].choices = Category.objects.all().values_list('name', 'name')
+
+
 class EditPostForm(forms.ModelForm):
+    category = forms.ChoiceField(choices=[])
+
     class Meta:
         model = Post
         fields = ['title', 'author', 'date', 'category', 'body']
         widgets = {
             'title': forms.TextInput(),
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'category': forms.Select(choices=choice_list),
             'body': MarkdownxFormField(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].choices = Category.objects.all().values_list('name', 'name')
