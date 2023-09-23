c171d18aa8450c5506a7054438b2178420857db1
loicb <loic.blanchardDev@gmail.com>
2023-09-23 19:45:45 +0800

    33 remove register (#34)
    * Update favicon
    
    * Add footer and remove registering

--- loicblog/loicblog/urls.py
+++ loicblog/loicblog/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('blog.urls')),
     path('members/', include('django.contrib.auth.urls')), # django built in auth urls: login/logout
-    path('members/', include('members.urls')), # if not django auth urls, go for our custom app urls: register
 ]
